b44aee6339663928275b731bb16080f6a3c79b85
lizlove <elizabethlovero@gmail.com>
2020-05-11 21:22:16 -0400

    Favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#210124`,
         theme_color: `#210124`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ct-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
