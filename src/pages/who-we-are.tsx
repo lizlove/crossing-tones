ebf497f78a725465148c4fe2de89f0acec58515b
lizlove <elizabethlovero@gmail.com>
2020-07-15 09:21:41 -0400

    Fix name spacing.

--- src/pages/who-we-are.tsx
+++ src/pages/who-we-are.tsx
@@ -10,36 +10,44 @@ const WhoWeAre = (props: PageProps) => (
     <SEO title="Who We Are" />
     <h1>Who we are</h1>
     <p>
-      Melissa Jones (Morristown NJ)Carnegie Mellon
-      <br></br> Jazz collector and independent researcher; specializes in 78
-      era. Vast experience of the offerings at Jazz at Lincoln Center. Mother of
-      modern Hot Club initiative.
+      <b>Melissa Jones</b> (Morristown NJ)
+      <br></br>
+      Jazz collector and independent researcher; specializes in 78 era. Vast
+      experience of the offerings at Jazz at Lincoln Center. Mother of modern
+      Hot Club initiative.
     </p>
     <p>
-      Joe Lizzi (Queens NY) Manhattan Coll ’88 <br></br> Audio engineer, record
-      producer, collector specializing in avant garde Jazz and modern
-      mainstream. Long-time studio engineer, now freelance for own and other
-      labels.
+      <b>Joe Lizzi</b> (Queens NY)
+      <br></br>
+      Audio engineer, record producer, collector specializing in avant garde
+      Jazz and modern mainstream. Long-time studio engineer, now freelance for
+      own and other labels.
     </p>
     <p>
-      Ben Young (Worcester MA and Brooklyn NY) Columbia ’92 <br></br> Researcher
-      and lecturer, in broadcast and classroom, broad range collector. Former
-      label staff producer, now freelance for own and other interests.
+      <b>Ben Young</b> (Worcester MA and Brooklyn NY)
+      <br></br>
+      Researcher and lecturer, in broadcast and classroom, broad range
+      collector. Former label staff producer, now freelance for own and other
+      interests.
     </p>
     <p>
-      Parker Fishel (Brooklyn NY) Columbia ’10 MS UTx ’14 <br></br> Archivist
-      and producer. Staff archivist for independent rock/roots interest and
-      producer of own ventures. Researching for bio-discography of Marion Brown.
+      <b>Parker Fishel</b> (Brooklyn NY)
+      <br></br>
+      Archivist and producer. Staff archivist for independent rock/roots
+      interest and producer of own ventures. Researching for bio-discography of
+      Marion Brown.
     </p>
     <p>
-      Charles Iselin (Chicago IL and Brooklyn NY) Columbia ‘15 <br></br> Civil
-      engineer and collector specializing in pre-war Jazz. Researching for
+      <b>Charles Iselin </b>(Chicago IL and Brooklyn NY)
+      <br></br>
+      Civil engineer and collector specializing in pre-war Jazz. Researching for
       discography of Adrian Rollini.
     </p>
     <p>
-      Matthew Rivera (NYC and Louisville KY) Columbia ‘18 <br></br> Filmmaker
-      and collector specializing in Jazz 78s. Operates own 78rpm DJ service
-      based on 78s.
+      <b>Matthew Rivera</b> (NYC and Louisville KY)
+      <br></br>
+      Filmmaker and collector specializing in Jazz 78s. Operates own 78rpm DJ
+      service based on 78s.
     </p>
   </Layout>
 );
