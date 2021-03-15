90fb4970f4d37463c393dfbf0eb270982d0eabc9
lizlove <elizabethlovero@gmail.com>
2021-03-14 20:23:38 -0400

    Add blank mission page.

---
+++ src/components/mission.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Mission = () => (
+  <div>
+    <h1>Mission Statement</h1>
+    <p>
+      This consortium called Crossing Tones proposes to act as custodians for collections of Jazz
+      music and related material.
+    </p>
+  </div>
+);
+
+export default Mission;
