b421ccb30891c3cef718dfb5af1e95587b54d789
lizlove <lizlove@users.noreply.github.com>
2022-11-22 00:47:51 -0600

    Add donate button.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,7 +7,6 @@ import Mission from '../components/mission';
 import Principles from '../components/principles';
 import Who from '../components/who';
 import Contact from '../components/contact-form';
-import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 const About = (props: PageProps) => (
   <Layout>
@@ -17,20 +16,18 @@ const About = (props: PageProps) => (
       <div className="grid-left">
         <a id="who"></a>
         <Who />
-        <a id="contact"></a>
+        {/* TODO: ADD DONATE LINK TO THIS BUTTON */}
+        {/* <a href="/" className="button button-full">Donate!</a> */}
       </div>
       <div className="grid-right">
         <a id="mission"></a>
         <Mission />
         <a id="principles"></a>
         <Principles />
-        {/* <AnchorLink className="anchorlink" to="/about#mission" title="Mission Statement" />
-        <AnchorLink className="anchorlink" to="/about#principles" title="First Principles" />
-        <AnchorLink className="anchorlink" to="/about#who" title="Who we are" />
-        <AnchorLink className="anchorlink" to="/about#contact" title="Contact" /> */}
       </div>
     </div>
     </section>
+    <a id="contact"></a>
     <Contact/>
   </Layout>
 );
