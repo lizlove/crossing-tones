5fa9ad66912e7887d4f8e6ccbeca4c875b512f60
lizlove <lizlove@users.noreply.github.com>
2022-11-21 23:33:00 -0600

    Migrate to Gatbsy v5 and make About page updates.

--- src/components/mission.js
+++ src/components/mission.js
@@ -4,7 +4,13 @@ const Mission = () => (
   <div>
     <h1>Mission Statement</h1>
     <p>
-    Crossing Tones acts as an intermediary custodian for significant collections of Jazz music and related items. We gather essential source materials and prepare them for acquisition, preservation, and use in meaningful teaching and listening initiatives. Through our efforts we hope to facilitate research, deepen appreciation, and broaden the listener base for the music.
+      Crossing Tones is a 501(c)(3) non-profit that acts as an
+      intermediary custodian for significant collections of Jazz music
+      and related items. We gather essential source materials and
+      prepare them for acquisition, preservation, and use in
+      meaningful teaching and listening initiatives. Through our
+      efforts we hope to facilitate research, deepen appreciation, and
+      broaden the listener base for the music.
     </p>
   </div>
 );
--- src/components/who.js
+++ src/components/who.js
@@ -6,43 +6,57 @@ const Who = () => (
     <p>
       <b>David Beal</b> (Queens, NY)
       <br></br>
-      Artist and musician. Collector of jazz 78s and co-leader of the Freehold
-      Hot Club.
+      Artist and musician. Collector of jazz 78s and co-leader of the
+      Freehold Hot Club.
     </p>
     <p>
       <b>Parker Fishel</b> (Brooklyn, NY)
       <br></br>
-      Archivist and producer. Staff archivist for independent rock/roots
-      interest and producer of own ventures. Researching for bio-discography of
-      Marion Brown.
+      Archivist and producer. Staff archivist for independent
+      rock/roots interest and producer of own ventures. Researching
+      for bio-discography of Marion Brown.
     </p>
     <p>
       <b>Charles Iselin </b>(Chicago, IL and Manhattan, NY)
       <br></br>
-      Structural engineer and collector specializing in pre-war Jazz. Custodian of the Phil Schaap radio archive.
+      Structural engineer and collector specializing in pre-war Jazz.
+      Custodian of the Phil Schaap radio archive.
     </p>
     <p>
       <b>Joe Lizzi</b> (Queens, NY)
       <br></br>
-      Audio engineer, record producer, collector specializing in avant garde
-      Jazz and modern mainstream. Long-time studio engineer, now freelance for
-      own and other labels.
+      Audio engineer, record producer, collector specializing in avant
+      garde Jazz and modern mainstream. Long-time studio engineer, now
+      freelance for own and other labels.
     </p>
     <p>
       <b>Matthew Rivera</b> (Brooklyn, NY and Louisville, KY)
       <br></br>
-      Filmmaker and collector specializing in Jazz 78s. Operates own 78rpm DJ
-      service based on 78s.
+      Filmmaker and collector specializing in Jazz 78s. Operates own
+      78rpm DJ service based on 78s.
+    </p>
+    <p>
+      <b>Katherine Whatley </b> (Palo Alto, CA and Tokyo, Japan)
+      <br></br>
+      Musician, writer and researcher. Performer of the koto
+      (transverse Japanese harp). PhD candidate at Stanford University
+      in Japanese literature and music. Japanese jazz and experimental
+      music scholar.
     </p>
     <p>
       <b>Ben Young</b> (Worcester, MA and Brooklyn, NY)
       <br></br>
       Researcher and lecturer, in broadcast and classroom, broad range
-      collector. Former label staff producer, now freelance for own and other
-      interests.
+      collector. Former label staff producer, now freelance for own
+      and other interests.
     </p>
     <p>
-      <b><i>Crossing Tones honors the legacy of Founding Member, Melissa Jones.</i></b>
+      <b>
+        <i>
+          Crossing Tones honors the legacy of Founding Member, Melissa
+          Jones.
+        </i>
+      </b>
     </p>
   </div>
 );
